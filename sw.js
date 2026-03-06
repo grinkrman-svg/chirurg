9c2379daf5112d838bdeb561f27e4f06d4dfaa28
grinkrman-svg <grinkrman@gmail.com>
2026-03-06 22:54:11 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,18 @@
-const CACHE_NAME = 'chirurg-v8';
+const CACHE = 'chirurg-v10';
 const FILES = [
-  './chirurg_desktop_v8.html',
-  './chirurg_mobile_v6.html'
+  './chirurg_desktop_v9.html',
+  './chirurg_mobile_v7.html'
 ];
-
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
